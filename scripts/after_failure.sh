9bcf7536340e1baa30c0ccfa58d0a3d1512706fa
Cat Haines <cat@pebble.com>
2016-03-16 11:04:39 -0700

    Added curl commands to after_{success|failure}.sh

--- scripts/after_failure.sh
+++ scripts/after_failure.sh
@@ -2,3 +2,4 @@
 set -e # halt script on error
 
 echo Failure.
+curl https://maker.ifttt.com/trigger/build_failure/with/key/$IFTTT_KEY
--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -2,3 +2,4 @@
 set -e # halt script on error
 
 echo Success!!
+curl https://maker.ifttt.com/trigger/build_success/with/key/$IFTTT_KEY
